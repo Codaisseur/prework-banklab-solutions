3862bb167938d9f9b7bc9aee826f1094770189a4
reinoptland <reinoptland@gmail.com>
2019-05-31 00:23:35 +0200

    Remove comments

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function menu(){
             withdraw()
             break
         
-        // Adding an option to the switch case statement
         case '3':
             deposit()
             break
@@ -48,9 +47,6 @@ function menu(){
     }
 }
 
-// we want to get an amount to deposit from the user
-// that's very similar to asking the user for an amount to withdraw
-// let's reuse that code by putting it into a function
 function getAmountInput(purpose){
     const input = prompt('How much would you like to ' + purpose + '?')
     
@@ -68,14 +64,11 @@ function getAmountInput(purpose){
     return amount
 }
 
-// displaying the new balance is also code we wrote before
-// let's reuse it as well
 function displayBalance(status){
     alert('Your ' + status + ' balance is: \n €' + balance)
 }
 
 function deposit(){
-    // using our getAmountInput function here
     const depositAmount = getAmountInput('deposit')
     
     balance = balance + depositAmount
@@ -84,7 +77,6 @@ function deposit(){
 }
 
 function withdraw(){
-    // using our getAmountInput function here as well
     const withdrawAmount = getAmountInput('withdraw')
 
     if(balance < withdrawAmount){
@@ -108,14 +100,7 @@ init()
  * [X] 4.) As a bank I want prevent users from withdrawing money don't have, so I don't go bankrupt.
  * [X] 5.) As a user I want to be able to choose if I want to know my balance or deposit money,
  *         So I can do what is relevant for me.
- * [ ] 6.) As a user I want to be able to choose to deposit money into my account, so It's safely stored.
- *         X Add an option to the menu for user to choose to deposit money
- *         X Handle the choice in the switch case statement
- *         X Call a function deposit
- *         X Ask the user how much he wants to deposit
- *         X Check if the input is valid (a number)
- *         X Add the money to the balance
- *         - Send the user back to the menu
+ * [X] 6.) As a user I want to be able to choose to deposit money into my account, so It's safely stored.
  * [ ] 7.) As a bank I want to prevent users from depositing more money in their account than they have,
  *         so I don't go bankrupt.
  * [ ] 8.) As user I want to log in using a username and password, so strangers don't withdraw my money.
