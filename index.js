4c70912936147e8aff7e9a56c10adab6d399dc5f
reinoptland <reinoptland@gmail.com>
2019-05-30 18:23:51 +0200

    Break down third story

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ init()
  * 
  * [X] 1.) As a user I want to know my current balance so I can make sound financial decisions
  * [X] 2.) As a bank I want to greet my user, so the user feels welcome.
- *         X Create a variable that stores the user's name
- *         X Greet the user when the program starts
  * [ ] 3.) As a user I want to be able to withdraw money from my account so 
  *         I can spend it on things I like.
+ *         - Change balance variable to something higher than 0
+ *         - Prompt the user for an amount they want to withdraw
+ *         - Subtract the amount from the balance variable
+ *         - Display the balance again
  * [ ] 4.) As a bank I want prevent users from withdrawing money don't have, so I don't go bankrupt.
  * [ ] 5.) As a user I want to be able to choose if I want to know my balance or deposit money,
  *         So I can do what is relevant for me.
