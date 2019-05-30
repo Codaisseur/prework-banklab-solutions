36368d667c26a20916f25c99b4d8d361310e45d9
reinoptland <reinoptland@gmail.com>
2019-05-30 17:58:26 +0200

    Write userstories, run program onload

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function init(){
-    console.log('hello')
+    console.log('hello')    
 }
+
+init()
+
+/** 
+ * Userstories
+ * 
+ * [ ] 1.) As a user I want to know my current balance so I can make sound financial decisions
+ * [ ] 2.) As a bank I want to greet my user, so the user feels welcome.
+ * [ ] 3.) As a user I want to be able to withdraw money from my account so 
+ *         I can spend it on things I like.
+ * [ ] 4.) As a bank I want prevent users from withdrawing money don't have, so I don't go bankrupt.
+ * [ ] 5.) As a user I want to be able to choose if I want to know my balance or deposit money,
+ *         So I can do what is relevant for me.
+ * [ ] 6.) As a user I want to be able to choose to deposit money into my account, so It's safely stored.
+ * [ ] 7.) As a bank I want to prevent users from depositing more money in their account than they have,
+ *         so I don't go bankrupt.
+ * [ ] 8.) As user I want to log in using a username and password, so strangers don't withdraw my money.
+*/
